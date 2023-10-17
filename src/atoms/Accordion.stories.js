b8f8252a669d6a60aa4747df049527a24a0de659
Erick Souza <erick_souza2104@outlook.com>
2023-10-17 20:10:00 -0300

    Component Accord

---
+++ src/atoms/Accordion.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Accordion from "./Accordion";
+
+export default {
+    title:"Components/Atoms/Accordion",
+    component: Accordion
+}
+export const usage = () => (
+    <Accordion>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit obcaecati perspiciatis aut iste ducimus qui. Officia, quos provident. Impedit expedita nesciunt natus eius sapiente fugiat?
+        </p>
+    </Accordion>
+)
